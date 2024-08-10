d6de29bf0ae5d61ed3eb29afe2779c19a2dc414e
coolbl0nde <mari.mix19@gmail.com>
2024-08-10 16:39:56 +0300

    changed theme and added activities package

---
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppingapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- ShoppingApp/app/src/main/java/com/example/shoppingapp/activities/ShoppingActivity.kt
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.shoppingapp
+package com.example.shoppingapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppingapp.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
