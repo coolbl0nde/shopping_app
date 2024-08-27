324437c514e2232207dd82bdfc9b83f52d6260e5
coolbl0nde <mari.mix19@gmail.com>
2024-08-27 19:16:30 +0300

    feat: added logic for login fragment

--- ShoppingApp/app/src/main/java/com/example/shoppingapp/fragments/loginRegister/LoginFragment.kt
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/fragments/loginRegister/LoginFragment.kt
@@ -1,7 +1,85 @@
 package com.example.shoppingapp.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.shoppingapp.R
+import com.example.shoppingapp.activities.ShoppingActivity
+import com.example.shoppingapp.databinding.FragmentLoginBinding
+import com.example.shoppingapp.util.Resource
+import com.example.shoppingapp.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            doneLoginButton.setOnClickListener {
+                val email = emailLoginEt.text.toString()
+                val password = passwordLoginEt.text.toString()
+
+                viewModel.login(email, password)
+            }
+
+            cancelLoginButton.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_authFragment)
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.login.collect {
+                    when(it){
+                        is Resource.Loading -> {
+                            //binding.doneLoginButton.startAnimation()
+                        }
+
+                        is Resource.Success -> {
+                            Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                                        Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                                startActivity(intent)
+                            }
+
+                            Log.d("test", it.data.toString())
+                            //binding.doneLoginButton.revertAnimation()
+                        }
+
+                        is Resource.Error -> {
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
+    }
 }
