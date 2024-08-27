7104dee8664b63883e57980c529966cc84442c6e
coolbl0nde <mari.mix19@gmail.com>
2024-08-27 19:25:58 +0300

    feat: added logic for auth fragment

--- ShoppingApp/app/src/main/java/com/example/shoppingapp/fragments/loginRegister/AuthFragment.kt
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/fragments/loginRegister/AuthFragment.kt
@@ -1,7 +1,35 @@
 package com.example.shoppingapp.fragments.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.shoppingapp.R
+import com.example.shoppingapp.databinding.FragmentAuthBinding
 
 class AuthFragment: Fragment(R.layout.fragment_auth) {
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAuthBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.registerAuthButton.setOnClickListener {
+            findNavController().navigate(R.id.action_authFragment_to_registerFragment)
+        }
+
+        binding.loginAuthButton.setOnClickListener {
+            findNavController().navigate(R.id.action_authFragment_to_loginFragment)
+        }
+    }
 }
