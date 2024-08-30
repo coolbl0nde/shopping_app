22deaa933600ff11e67bf2d341ce0d9588e1ac40
coolbl0nde <mari.mix19@gmail.com>
2024-08-30 23:15:51 +0300

    feat: added bottom nav for shopping activity

---
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.shoppingapp.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.shoppingapp.R
+import com.example.shoppingapp.databinding.FragmentAuthBinding
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+}
